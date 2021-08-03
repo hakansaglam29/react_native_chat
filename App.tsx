8f260249eaa3dc41fa358fa20c81948cec604d4f
hakansaglam <hakan.saglam@qconnex.com>
2021-08-03 14:36:12 +0200

    socket.io message handling added

--- App.tsx
+++ App.tsx
@@ -1,21 +1,51 @@
-import React, {useEffect} from 'react';
-import axios from 'axios';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+//import axios from 'axios';
+import {io} from 'socket.io-client';
+
+import {View, Text, SafeAreaView, Button} from 'react-native';
 
 const App = () => {
-  const fetchData = async () => {
-    let response = await axios.get('http://localhost:3000');
-    console.log('response', response.data);
+  // const fetchData = async () => {
+  //   let response = await axios.get('http://localhost:3000');
+  //   console.log('response', response.data);
+  // };
+  const socket = io('http://localhost:3000', {
+    transports: ['websocket'],
+    forceNew: true,
+    upgrade: false,
+  });
+
+  const [state, setState] = useState([]);
+
+  const receiveMessage = () => {
+    socket.on('msgToClient', ahmet => {
+      console.log('ahmet', ahmet);
+      setState(ahmet);
+    });
+  };
+
+  const sendMessage = () => {
+    socket.emit('msgToServer', 'at basi');
+  };
+
+  const deleteMessage = () => {
+    socket.emit('msgToServer', 'delete');
   };
 
   useEffect(() => {
-    fetchData();
+    receiveMessage();
   }, []);
 
   return (
-    <View>
-      <Text />
-    </View>
+    <SafeAreaView>
+      <View>
+        {state.map(item => (
+          <Text>{item}</Text>
+        ))}
+      </View>
+      <Button title="Add new lamp" onPress={sendMessage} />
+      <Button title="delete lamp" onPress={deleteMessage} />
+    </SafeAreaView>
   );
 };
 
